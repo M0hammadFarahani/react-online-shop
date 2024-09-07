2257508c65ca3cf669d6c2eb8a0fafb49911bcdc
Mohammad <mmdwfrh@gmail.com>
2024-09-07 17:15:48 +0330

    Routes and their components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Shop from './pages/shop/shop'
+import Cart from './pages/cart/cart'
+import Navbar from './components/navbar'
 
 
 
@@ -8,7 +12,13 @@ function App() {
 
   return (
     <>
-    
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Shop />} />
+        <Route path='/cart' element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
