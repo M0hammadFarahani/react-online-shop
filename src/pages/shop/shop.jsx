d6401d2e76c32e09b7fea7478a8e9d91d5d224e0
Mohammad <mmdwfrh@gmail.com>
2024-09-09 10:58:08 +0330

    Loading feature added

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -10,33 +10,48 @@ import 'react-toastify/dist/ReactToastify.css';
 function Shop({ search, handleBuy }) {
 
     const [products, setProducts] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
 
     useEffect(() => {
+        setIsLoading(true)
         fetch('https://fakestoreapi.com/products')
         .then(res => res.json())
-        .then(data => setProducts(data))
+        .then(data => {
+            setIsLoading(false)
+            setProducts(data)
+        })
     }, [])
 
     return (
         <div className="container">
             <h1 className="text-center my-5">Shop</h1>
-            <div className="row">
-                {
-                    products.filter((item) => {
-                        return search.toLowerCase() === '' ? item : item.title.toLowerCase().includes(search)
-                    }).map((item) => (
-                        <div className="col-lg-4 col-md-6 p-5" key={item.id} style={{height: '500px'}} >
-                            <img src={item.image} className="w-100 h-50 mb-4" />
-                            <h5>{item.title}</h5>
-                            <p className="fw-bold mb-4">${item.price}</p>     
-                            <div className="btn-group">
-                                <button className="btn btn-success" onClick={() => handleBuy(item)}>Buy</button>
-                                <Link className="btn btn-warning" to={`/detail/${item.id}`}>Detail</Link>
+            {
+                isLoading ?
+                <div class="d-flex justify-content-center">
+                    <div class="spinner-border mt-5" role="status">
+                        <span class="sr-only"></span>
+                    </div>
+                </div>
+                :
+                <div className="row">
+                    {
+                        products.filter((item) => {
+                            return search.toLowerCase() === '' ? item : item.title.toLowerCase().includes(search)
+                        }).map((item) => (
+                            <div className="col-lg-4 col-md-6 p-5" key={item.id} style={{height: '500px'}} >
+                                <img src={item.image} className="w-100 h-50 mb-4" />
+                                <h5>{item.title}</h5>
+                                <p className="fw-bold mb-4">${item.price}</p>     
+                                <div className="btn-group">
+                                    <button className="btn btn-success" onClick={() => handleBuy(item)}>Buy</button>
+                                    <Link className="btn btn-warning" to={`/detail/${item.id}`}>Detail</Link>
+                                </div>
                             </div>
-                        </div>
-                    ))
-                }
-            </div>
+                        ))
+                    }
+                </div>
+            }
+            
             <ToastContainer/>
         </div>
     )
